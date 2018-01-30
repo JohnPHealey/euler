ea5aa170e1bc4c1c3431aa8bf6d00b95fcb4ad8e
JohnPHealey <jph432@mun.ca>
2018-01-30 15:45:49 -0330

    Update PowerDigitSum.java

--- Solved/PowerDigitSum.java
+++ Solved/PowerDigitSum.java
@@ -1,11 +1,16 @@
-package beginner.Solved;
+package Solved;
 
 import java.math.BigInteger;
 
+/**
+* Solution to Problem #16 on Project Euler
+* Finds the sum of the digits of the number 2^1000
+* @author: John Healey, 2016-08-24
+*/
 public class PowerDigitSum {
 	public static void main(String [] args) {
 		BigInteger number = new BigInteger("2");
-		number = number.pow(1000);
+		number = number.pow(1000); //:D
 		System.out.println(number.toString());
 		String word = number.toString();
 		int sum = 0;
