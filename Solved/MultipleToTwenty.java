74d82edea0e84a5700acb3fcab06b72aab02693b
JohnPHealey <jph432@mun.ca>
2018-01-30 15:41:04 -0330

    Update MultipleToTwenty.java

--- Solved/MultipleToTwenty.java
+++ Solved/MultipleToTwenty.java
@@ -1,5 +1,10 @@
 package beginner.Solved;
 
+/**
+* Solution to Problem #5 on Project Euler
+* Finds the smallest positive number that is evenly divisible by all of the numbers from 1 to 20
+* @author: John Healey, 2016-08-15
+*/
 public class MultipleToTwenty {
 	public static void main(String [] args) {
 		
